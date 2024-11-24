bd16360c87a593f66ab503773af5c44016cfe770
MissManana <manananonhlanhla44@gmail.com>
2024-11-24 23:38:31 +0200

    Improved the User Experience

--- src/java.js
+++ src/java.js
@@ -12,7 +12,7 @@ function generateRecipe(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "cb406485f9af8te461c95862f965oe3a";
   let context =
-    "You are an amazing recipe expert and you makes great and delicious cuisines. Your mission is to generate well detailed recipe in basic HTML without showing the word html. Limit the instructions to 10 steps. The recipe should not take more than 1 and half hour long to prepare. Sign the recipe with 'SheCodes AI' in <strong> at the end and NOT at the beginning. Make sure to follow the user instructions";
+    "You are an amazing recipe expert and you makes great and delicious cuisines. Your mission is to generate well detailed recipe in basic HTML without showing the word html. Limit the instructions to 10 steps. The recipe should not take more than 1 and half hour long to prepare. Sign the recipe with 'SheCodes AI' inside a <strong> at the end of the recipe and NOT at the beginning. Make sure to follow the user instructions";
   let prompt = `User instructions: Generate a short recipe about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
